ea70b833ffda3c55972ac5d0ac015c5bf7b95821
brooke kullberg and megan schulte <megan@>
2019-03-28 16:34:16 -0700

    add more if statements and css styling

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@ $(document).ready(function() {
     // alert("hi");
 
     var age = parseInt($("input").val());
+    var pants = $("#pants").val();
 
-    if (age < 15) {
+    if (age < 15 && pants === "jeans") {
       $("#young").show();
       $("#middle, #dolly").hide();
-    } else if (age >= 15 && age < 35) {
-      $("#middle").show();
-      $("#young, #dolly").hide();
+    } else if (age >= 15 && age < 35 && pants === "cargo") {
+      $("#dolly").show();
+      $("#young, #middle").hide();
+    } else if (age >= 15 && age < 35 ) {
+        $("#middle").show();
+        $("#young, #dolly").hide();
     } else if (age >= 35) {
       $("#dolly").show();
       $("#middle, #young").hide();
